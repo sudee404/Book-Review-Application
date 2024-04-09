efbf92fb2af60ccb14cdf2ea4ea8069a53a24bc6
sudee404 <sudaise18@gmail.com>
2024-04-09 03:29:19 +0300

    Add new page for creating book clubs and implement club banner component

---
+++ frontend/app/(books)/book-clubs/new/page.js
@@ -0,0 +1,16 @@
+"use client";
+import * as React from "react";
+import Box from "@mui/material/Box";
+import Divider from "@mui/material/Divider";
+import FAQ from "../../../../components/sections/FAQ";
+import ClubForm from "../../../../components/clubs/ClubForm";
+
+export default function Page() {
+	return (
+		<Box>
+			<ClubForm />
+			<Divider />
+			
+		</Box>
+	);
+}
